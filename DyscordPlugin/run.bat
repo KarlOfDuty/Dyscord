8103b758c9a03323a40ce8b1584a9c33d2a0c465
Karl Essinger <xkaess22@gmail.com>
2018-09-06 23:53:35 +0200

    Some more structural fixes

--- DyscordPlugin/run.bat
+++ DyscordPlugin/run.bat
@@ -1,3 +1,3 @@
 spcomp Dyscord.sp
-copy /Y Dyscord.smx ..\..\dystopiaserver\dystopia\addons\sourcemod\plugins
-..\..\dystopiaserver\bin\win32\srcds.exe  -console +map dys_fortress -port 27016
+copy /Y Dyscord.smx ..\..\..\dystopiaserver\dystopia\addons\sourcemod\plugins
+..\..\..\dystopiaserver\bin\win32\srcds.exe  -console +map dys_fortress -port 27016
